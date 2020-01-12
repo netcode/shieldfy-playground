a6177ac91480c3d14be47cbeb43a4f1557838c5b
Eslam Salem <eslam@shieldfy.com>
2020-01-12 10:26:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    let name = req.query.name;
+    let name = getName(req);
     res.send(`Hello, ${name}!`)
 })
+
+const getName = (inputs) => {
+    let name = inputs.query.name;
+    return name;
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
